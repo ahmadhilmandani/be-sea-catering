b0562c061efa0d035f38e8efe830d11193b61691
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-07-18 10:04:08 +0700

    tes

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use('/api/admin/dashboard-data', adminDashboardRoute)
 app.use(responseSuccesHandler)
 app.use(errorHanlder)
 
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
-app.listen(SERVER_PORT, () => {
-  console.log(`running on http://localhost:${SERVER_PORT}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`running on http://localhost:${PORT}`)
 })
