185610714109d290b81678210f641f974ef30a76
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-07-20 17:15:16 +0700

    te

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 const cors = require('cors')
 
-if (process.env.APP_MODE != 'production') {
+if (process.env.APP_MODE == 'production') {
   app.use(cors({
     origin: 'https://fe-sea-catering.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
